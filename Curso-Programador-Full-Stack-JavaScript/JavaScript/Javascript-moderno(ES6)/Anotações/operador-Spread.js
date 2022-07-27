94939b1972c3c4334bce61063fbf3120a1f044b8
Bruno Henrique Lippert <brunolippert2002@gmail.com>
2022-07-27 18:13:19 -0300

    correção nas anotações do operador spread

--- Curso-Programador-Full-Stack-JavaScript/JavaScript/Javascript-moderno(ES6)/Anotações/operador-Spread.js
+++ Curso-Programador-Full-Stack-JavaScript/JavaScript/Javascript-moderno(ES6)/Anotações/operador-Spread.js
@@ -3,7 +3,7 @@
 
 let spaceships = ["Colossus", "Ártemis", "Fênix"]
 
-console.log(...spaceships)                  //os três pontos(...) são para o console.log() entender que os paremetros que serão passados estão em um array e retornar 3 strings
+console.log(...spaceships)                  //os três pontos(...) são para a função entender que os paremetros que serão passados estão em um array e retornar 3 strings
 
 //Outros exemplos
 let newSpaceships = [...spaceships, "Puller"]       //seria a mesma coisa que -> let newSpaceships = ["Colossus", "Ártemis", "Fênix", "Puller"], se não colocarmos os três pontos ele vai ser considerado como um array e ira retornar: [["Colossus", "Ártemis", "Fênix"], "Puller"]
