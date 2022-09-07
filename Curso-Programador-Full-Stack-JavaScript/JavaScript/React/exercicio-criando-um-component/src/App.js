b5e265875274e66b1c093600df31c20b433470d8
Bruno Henrique Lippert <brunolippert2002@gmail.com>
2022-09-07 17:43:32 -0300

    exercício componenets react

---
+++ Curso-Programador-Full-Stack-JavaScript/JavaScript/React/exercicio-criando-um-component/src/App.js
@@ -0,0 +1,13 @@
+import HelloWorld from "./components/Hello";
+import Lorem from "./components/lorem";
+
+function App() {
+  return (
+    <div>
+      <HelloWorld/>
+      <Lorem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Curso-Programador-Full-Stack-JavaScript/JavaScript/React/exercicio-criando-um-component/src/components/Hello.js
@@ -0,0 +1,12 @@
+
+function HelloWorld() {
+    return <h1>Hello World</h1>; //COMPONENTE DE FUNÇÃO
+}
+
+// class HelloWorld extends ReadableByteStreamController.Component {
+//     render () {
+//         return <h1>Hello World</h1>;  COMPONENTE DE CLASSE
+//     }
+// }
+
+export default HelloWorld; //exportando o arquivo
