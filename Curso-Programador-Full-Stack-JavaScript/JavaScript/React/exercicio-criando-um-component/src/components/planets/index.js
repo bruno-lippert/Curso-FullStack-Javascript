297c41d30ab5a02d71531621aa383345f6a90efb
Bruno Henrique Lippert <brunolippert2002@gmail.com>
2022-09-08 20:04:28 -0300

    props

--- Curso-Programador-Full-Stack-JavaScript/JavaScript/React/exercicio-criando-um-component/src/components/planets/index.js
+++ Curso-Programador-Full-Stack-JavaScript/JavaScript/React/exercicio-criando-um-component/src/components/planets/index.js
@@ -1,19 +1,28 @@
 import React, {Fragment} from 'react';
 import Planet from './planet';
 
+const clickOnPlanet = (name) => {
+    alert(`Um click no planeta ${name}`)
+}
+
 const Planets = () => {
     return (
         <Fragment>
             <h3>Planet List</h3>
+            <button>Show message</button>
             <Planet name = "Mercúrio"
                     description = "Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar."
                     img_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"
-                    link = "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"/>
+                    link = "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
+                    clickOnPlanet={clickOnPlanet}
+                    />
             
             <Planet name = "Plutão"
                     description = "Plutão, formalmente designado 134340 Plutão (símbolos: ⯓ e ♇) é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol."
                     img_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
-                    link = "https://pt.wikipedia.org/wiki/Plut%C3%A3o"/>
+                    link = "https://pt.wikipedia.org/wiki/Plut%C3%A3o"
+                    clickOnPlanet={clickOnPlanet}
+                    />
                     
         </Fragment>
     )
--- Curso-Programador-Full-Stack-JavaScript/JavaScript/React/exercicio-criando-um-component/src/components/planets/planet/index.js
+++ Curso-Programador-Full-Stack-JavaScript/JavaScript/React/exercicio-criando-um-component/src/components/planets/planet/index.js
@@ -2,7 +2,7 @@ import DescriptionWithLink from '../../shared/descriptionWithLink';
 import GrayImg from '../../shared/gray_img';
 const Planet = (props) => {
     return (
-        <div>
+        <div onClick={() => props.clickOnPlanet(props.name)}>
             <h4>{props.name}</h4>
             <DescriptionWithLink desciption = {props.desciption} link={props.link}/>
             <GrayImg img_url={props.img_url} />
