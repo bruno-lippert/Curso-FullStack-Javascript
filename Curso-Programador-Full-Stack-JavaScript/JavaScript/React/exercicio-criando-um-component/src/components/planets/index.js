08d9eee4eaf539f6803a0e6bfddf7ed2c6209d40
Bruno Henrique Lippert <brunolippert2002@gmail.com>
2022-09-14 17:38:10 -0300

    att

--- Curso-Programador-Full-Stack-JavaScript/JavaScript/React/exercicio-criando-um-component/src/components/planets/index.js
+++ Curso-Programador-Full-Stack-JavaScript/JavaScript/React/exercicio-criando-um-component/src/components/planets/index.js
@@ -1,31 +1,73 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import Planet from './planet';
 
 const clickOnPlanet = (name) => {
     alert(`Um click no planeta ${name}`)
 }
 
-const Planets = () => {
-    return (
-        <Fragment>
-            <h3>Planet List</h3>
-            <button>Show message</button>
-            <Planet name = "Mercúrio"
-                    description = "Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar."
-                    img_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"
-                    link = "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
-                    clickOnPlanet={clickOnPlanet}
-                    />
-            
-            <Planet name = "Plutão"
-                    description = "Plutão, formalmente designado 134340 Plutão (símbolos: ⯓ e ♇) é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol."
-                    img_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
-                    link = "https://pt.wikipedia.org/wiki/Plut%C3%A3o"
-                    clickOnPlanet={clickOnPlanet}
-                    />
-                    
-        </Fragment>
-    )
+async function getPlanets() {
+    let response = await fetch('http://localhost:3000/api/planets.json') //serve para chamar uma api, nesse caso estamos chamando direto do arquivo
+    let data = await response.json() //para receber o resultado da chamada da api
+    return data
+};
+
+class Planets extends React.Component {
+    constructor(props) {
+        super(props); // o metodo super é vai passar as props para a classe que estamos erdando(React.Component)
+        this.state = { //sepre que o state atualiza, nosso component é re-renderizado
+            planets: [ ]
+        }
+    }
+
+    componentDidMount() {   //roda quando o component for exibido
+        getPlanets().then(data => {
+            this.setState(state => ({   //depois de baixar as infos da api
+                planets: data['planets'] //e colocamos as infos dentro de planets
+                                        //o 'data['planets']' é para pegas as infos que estão dentro de planets na api
+            }))
+        })
+    }
+
+    removeLast = () => {
+        let new_planets = [...this.state.planets] //o operador spread(...), extrai todas as propriedades de planets
+        new_planets.pop()
+        this.setState(state => ({
+            planets: new_planets
+        }))
+    }
+
+    duplicateLastPlanet = () => {
+        let last_planet = this.state.planets[this.state.planets.length -1]
+        this.setState(state => ({
+            planets: [...this.state.planets, last_planet]
+        }))
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <h3>Planet List</h3>
+                <button onClick={this.removeLast}>Remove last</button>
+                <button onClick={this.duplicateLastPlanet}>Duplicate last</button>
+                {this.state.planets.map((planet, index) => 
+                <Planet
+                    id={planet.id}
+                    name={planet.name}
+                    description={planet.description}
+                    img_url={planet.img_url}
+                    link={planet.link}
+                    key={index}
+                    // clickOnPlanet={clickOnPlanet}
+                    // title_with_underline={true}
+                />
+                )}
+
+            </Fragment>
+        )
+
+    }
 }
+
+
 
 export default Planets;
--- Curso-Programador-Full-Stack-JavaScript/JavaScript/React/exercicio-criando-um-component/src/components/planets/planet/index.js
+++ Curso-Programador-Full-Stack-JavaScript/JavaScript/React/exercicio-criando-um-component/src/components/planets/planet/index.js
@@ -1,13 +1,61 @@
-import DescriptionWithLink from '../../shared/descriptionWithLink';
-import GrayImg from '../../shared/gray_img';
-const Planet = (props) => {
-    return (
-        <div onClick={() => props.clickOnPlanet(props.name)}>
-            <h4>{props.name}</h4>
-            <DescriptionWithLink desciption = {props.desciption} link={props.link}/>
-            <GrayImg img_url={props.img_url} />
-        </div>
-    )
+import React from "react";
+import DescriptionWithLink from "../../shared/descriptionWithLink";
+import GrayImg from "../../shared/gray_img";
+
+async function getSatellites(id) {
+    let response = await fetch(`http://localhost:3000/api/${id}.json`); //serve para chamar uma api, nesse caso estamos chamando direto do arquivo
+    let data = await response.json(); //para receber o resultado da chamada da api
+    return data;
+}
+
+class Planet extends React.Component {
+    constructor(props) {
+        super(props); // o metodo super é vai passar as props para a classe que estamos erdando(React.Component)
+        this.state = {
+            //sepre que o state atualiza, nosso component é re-renderizado
+            satellites: [],
+        };
+    }
+
+    componentDidMount() {   //roda quando o component for exibido
+        getSatellites(this.props.id).then((data) => {
+            this.setState((state) => ({
+                //depois de baixar as infos da api
+                satellites: data["satellites"] //e colocamos as infos dentro de planets
+                //o 'data['planets']' é para pegas as infos que estão dentro de planets na api
+            }));
+        });
+    }
+
+    render() {
+        let title;
+        if (this.props.title_with_underline) {
+            title = (
+                <h4>
+                    <u>{this.props.name}</u>
+                </h4>
+            );
+        } else {
+            title = <h4>{this.props.name}</h4>;
+        }
+        return (
+            <div onClick={() => this.props.clickOnPlanet(this.props.name)}>
+                {this.props.title}
+                <DescriptionWithLink
+                    description={this.props.description}
+                    link={this.props.link}
+                />
+                <GrayImg img_url={this.props.img_url} gray={this.props.gray} />
+
+                <h4>Satélites</h4>
+                <ul>
+                {this.state.satellites.map((satellite, index) => 
+                    <li key={index}>{satellite.name}</li>
+                )}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default Planet;
