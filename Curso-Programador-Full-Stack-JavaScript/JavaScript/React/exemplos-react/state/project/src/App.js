8311ba36aeba0f13d3abcba0b59640e8b317a00b
Bruno Henrique Lippert <brunolippert2002@gmail.com>
2022-09-28 19:44:25 -0300

    state

---
+++ Curso-Programador-Full-Stack-JavaScript/JavaScript/React/exemplos-react/state/project/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react'
+
+function App() {
+  const [contador, setContador] = useState(0)
+  function incrementar() {
+    setContador(contador+1)
+  }
+  return (
+    <div>
+      <h1>{contador}</h1>
+      <button onClick={incrementar}>Aumentar</button>
+    </div>
+  );
+}
+
+export default App;
